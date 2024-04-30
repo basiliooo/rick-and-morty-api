5a80966062ccbabb3a27818985fdfb66e48c4046
Nicolas Basílio Golino <125198885+basiliooo@users.noreply.github.com>
2024-04-29 23:00:34 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,20 @@ let num = ref(0)
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
-    <div class="container-fluid ">
-      <router-link class="navbar-brand" to="/">Rick And Morty</router-link>
-    </div>
-  </nav>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link class="navbar-brand" to="/">Rick and Morty</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/about">Sobre</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
   <div class="main">
     <router-view></router-view>
   </div>
