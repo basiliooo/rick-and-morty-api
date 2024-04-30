6ed3279bfb1d4f2bf523c819c9795cc87e3063e9
Nicolas Basílio Golino <125198885+basiliooo@users.noreply.github.com>
2024-04-29 23:00:03 -0300

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class=" about bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white" style="background-image: url('https://cinepop.com.br/wp-content/uploads/2022/05/rick-e-morty.jpg'); height:  100vh;"
+  >
+  <div class="mask" style="background-color: rgba(0, 0, 0, 0.6); height: 100vh;">
+  <h1 class="about">Rick and Morty API</h1>
+  <div class="info">
+  <h3>RA: 1967759</h3>
+  <h3>Aluno: Nicolas Basílio Golino</h3>
+  <h3>Curso: ADS - C </h3>
+  </div>
+  </div>
   </div>
 </template>
 
 <style>
 @media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+    text-align: center;
+  }
+
+  .mask {
+    padding-top: 30vh;
   }
 }
 </style>
